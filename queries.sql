555a0621f95640d9e686e0b2000593eb27c0b1b0
John Youn <john@Johns-MacBook-Pro.local>
2020-11-04 15:35:41 -0800

    queries

--- queries.sql
+++ queries.sql
@@ -2,8 +2,42 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select
+    p.productname,
+    c.categoryname
+from product as p
+join category as c
+    on p.categoryid = c.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select 
+    o.id as orderID,
+    s.companyname as companyname
+from [order] as o
+join shipper as s
+on o.shipvia = s.id
+    where o.OrderDate < '2012-08-09'
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select
+    p.productname,
+    o.quantity
+from product as p
+join orderdetail as o
+    on o.ProductId = p.id
+    where o.orderid = '10251'
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select
+    o.id,
+    c.companyname,
+    e.lastname
+from [order] as o
+join customer as C
+    on o.customerid = c.id
+join employee as e
+    on o.employeeid = e.id

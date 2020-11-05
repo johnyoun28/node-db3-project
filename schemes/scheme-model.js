dcf682911e7494c010b0feb976e4fdf3c048c5ec
John Youn <john@Johns-MacBook-Pro.local>
2020-11-04 17:35:28 -0800

    models done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,26 @@
 // scheme-model
+const db = require('../data/db-config')
+
+module.exports = {
+    find() {
+        return db('schemes')
+    },
+    findById(id) {
+        return db('schemes').where({ id }).first()
+    },
+    findSteps(id) {
+        return db('schemes as s')
+        .join('steps as t', 's.id', 't.scheme_id')
+        .select('s.scheme_name', 't.step_number', 't.instructions')
+        .where({'s.id': id})
+    },
+    add(scheme) {
+        return db('schemes').insert(acc)
+    },
+    update(changes, id) {
+        return db('schemes').where({ id }).update(acc)
+    },
+    remove(id) {
+        return db('schemes').where({ id }).del()
+    }
+}
